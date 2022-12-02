25275ca57843274c9adf4064299b4ecbbf446c53
mnlsmmdv <mnlsmmdv13@gmail.com>
2022-12-03 01:48:45 +0500

    Dislpays Hello World message

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+ * Name: Ahmed Affaan
+ * Title: Program.cs
+ * Folder: HelloWorld
+ * Date: 02/12/2022
+ * Country: Republic of Maldives
+ * Code version: -
+ * Description: -
+ * Credits to: Bro Code
+ * Credits to link: https://www.youtube.com/watch?v=6MAufQ6vGtI&list=PLZPZq0r_RZOOkUQbat8LyQii36cJf2SWT&index=14
+ * Note: Uncomment codes to execute and comment them when not in use.
+ */
+
+// Using system library in project.
+using System;
+
+namespace HelloWorld{
+    // Main class.
+    class Program{
+        // Main function.
+        static void Main(String[] args){
+            // Printing a message.
+            Console.WriteLine("Hello World!\nHello Affaan.");
+        }
+    }
+}
