3acabfc3a5d8a57cb8e177379dbcf93819a3d592
mnlsmmdv <mnlsmmdv13@gmail.com>
2022-12-03 02:49:34 +0500

    New file created

---
+++ Variables/Variables/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+ * Name: Ahmed Affaan
+ * Title: Program.cs
+ * Folder: C-Sharp/Variables
+ * Date: 03/12/2022
+ * Country: Republic of Maldives
+ * Code version: -
+ * Description: -
+ * Credits to: Bro Code
+ * Credits to link: -
+ * Note: Uncomment codes to execute and comment them when not in use.
+ */
+
+// Using system libraries.
+using System;
+
+namespace Variables{
+    // Main class.
+    class Program{
+        // Main function.
+        static void Main(string[] args){
+            // Printing a message.
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
