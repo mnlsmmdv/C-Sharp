62edc34b02b9c659973d8c6ef7691f23dcb4f21b
mnlsmmdv <mnlsmmdv13@gmail.com>
2022-12-03 15:05:01 +0500

    New file created

---
+++ Cryptography_Self_Learn/PasswordManager/PasswordManager/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+ * Name: Ahmed Affaan
+ * Title: Program.cs
+ * Folder: C-Sharp/Cryptography_Self_Learn/PasswordManager
+ * Date: 03/12/2022
+ * Country: Republic of Maldives
+ * Code version: -
+ * Description: -
+ * Credits to: Tech With Tim
+ * Credits to link: 
+ * Note: Uncomment codes to execute and comment them when not in use.
+ */
+
+// Using system library.
+using System;
+
+namespace PasswordManager{
+    // Main class.
+    class Program{
+        // Main function.
+        static void Main(string[] args){
+            // Printing a message.
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
